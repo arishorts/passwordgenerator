6b29e4b14256ee99d75d5ce989893bc93ef5d7d6
arishorts <arieljschwartz@gmail.com>
2022-12-29 14:05:43 -0700

    Added random charset generation, validation, and simulated user input

---
+++ Develop/script.js
@@ -0,0 +1,67 @@
+let length = 9;
+let charset = "";
+let lowercase_chars = false;
+let uppercase_chars = false;
+let number_chars = false;
+let special_chars = false;
+
+userInput();
+function userInput() {
+  length = 8;
+  lowercase_chars = false;
+  uppercase_chars = false;
+  number_chars = true;
+  special_chars = true;
+}
+
+function generateCharset() {
+  if (number_chars) {
+    charset += "1234567890";
+  }
+  if (lowercase_chars) {
+    charset += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (uppercase_chars) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (special_chars) {
+    charset += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  } else {
+    charset = "";
+  }
+}
+generatePassword();
+
+// Assignment code here
+function generatePassword() {
+  //ask for user input
+  userInput();
+  generateCharset();
+  if (charset == "") {
+    return "At least one type of character must be selected";
+  }
+  if (length < 8) {
+    return "Password must be over 8 chars";
+  }
+  const passwordLength = length;
+  let passwordContent = "";
+  for (index = 0; index < passwordLength; index++) {
+    const randomValue = Math.floor(Math.random() * charset.length);
+    passwordContent += charset.charAt(randomValue);
+  }
+  return passwordContent;
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
